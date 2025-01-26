65f63870526a83c364f76b5e7f589c6450405d9e
Adarsh <akesherwani900@gmail.com>
2025-01-26 14:06:33 +0530

    major changes

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  assetsInclude: ['**/*.jpg', '**/*.jpeg', '**/*.png', '**/*.gif', '**/*.webp', '**/*.svg', '**/*.JPG'],
-})
